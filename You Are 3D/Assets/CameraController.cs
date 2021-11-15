564f4c0f4799c98043b137d047cbe8ff432ec1c8
MicahRP <85714913+MicahRP@users.noreply.github.com>
2021-11-15 16:52:57 -0500

    I added the ability to control the camera in a scene
    Use q and e to rotate the camera left and right around the player

--- You Are 3D/Assets/CameraController.cs
+++ You Are 3D/Assets/CameraController.cs
@@ -6,6 +6,8 @@ public class CameraController : MonoBehaviour
 {
     public GameObject player;
     private Vector3 offset;
+    public float rotateSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +19,18 @@ public class CameraController : MonoBehaviour
     void LateUpdate()
     {
         transform.position = player.transform.position + offset;
+
+        // Spin the object around the target at rotateSpeed degrees/second.
+        if (Input.GetKey("q"))
+        {
+            transform.RotateAround(player.transform.position, Vector3.up, (rotateSpeed) * Time.deltaTime);
+        }
+        if (Input.GetKey("e"))
+        {
+            transform.RotateAround(player.transform.position, Vector3.up, (-rotateSpeed) * Time.deltaTime);
+        }
+
+        offset = transform.position - player.transform.position;
 
     }
 }
