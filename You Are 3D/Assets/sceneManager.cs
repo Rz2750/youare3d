e88a54bb80edd48a73e7d6479f8052bfb1b6edca
hannahshaby <hannahshaby@gmail.com>
2021-11-14 19:47:08 -0500

    added a reset button
    VERY STRANGE BUG MAKES SHADING LOOK STRANGE AFTER RESETTING ONCE???????

--- You Are 3D/Assets/sceneManager.cs
+++ You Are 3D/Assets/sceneManager.cs
@@ -7,8 +7,11 @@ using UnityEngine.SceneManagement;
 public class sceneManager : MonoBehaviour
 {
         public void OnButtonPress(){
-            Debug.Log("You have pressed the button");
             SceneManager.LoadScene("sampleMaze"); //change to title of next scene to link
+        }
+        
+        public void OnRestartPress(){
+            SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex);//change to title of next scene to link
         }
 }
 
