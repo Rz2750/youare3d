288217195b661afcab9cb96e12bddb1cd253c3ae
MicahRP <85714913+MicahRP@users.noreply.github.com>
2021-11-29 15:56:46 -0500

    Key now bobs up and down
    Still has no functionality

---
+++ You Are 3D/Assets/scripts/keyAnimation.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class keyAnimation : MonoBehaviour
+{
+    float originalY;
+
+    public float floatStrength = 1; // You can change this in the Unity Editor to 
+                                    // change the range of y positions that are possible.
+    public float floatSpeed = 1;
+    void Start()
+    {
+        this.originalY = this.transform.position.y;
+    }
+
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x,
+            originalY + (Mathf.Sin(Time.time * floatSpeed) * floatStrength),
+            transform.position.z);
+    }
+}
