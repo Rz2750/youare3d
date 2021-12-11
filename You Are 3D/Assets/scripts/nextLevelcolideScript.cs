4f8c4f406cff8cf516a50231d90452bfefabe79d
Aayushma Gautam <gtmaayushma@gmail.com>
2021-12-11 15:59:06 -0500

    flag rotates now

--- You Are 3D/Assets/scripts/nextLevelcolideScript.cs
+++ You Are 3D/Assets/scripts/nextLevelcolideScript.cs
@@ -1,3 +1,42 @@
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.SceneManagement;
+
+
+
+//public class nextLevelcolideScript : MonoBehaviour
+//{
+//    private Scene scene;
+//    void Start()
+//    {
+//        scene = SceneManager.GetActiveScene();
+//    }
+
+//    private void OnTriggerEnter(Collider other)
+//    {
+//        Debug.Log("rotate");
+
+
+//        transform.Rotate(new Vector3(0f, 0f, 100f)*Time.deltaTime);
+//        StartCoroutine(Example());
+
+//        if (scene.name == "Level 0") {
+//            SceneManager.LoadScene("Level 1");
+//        } else if (scene.name == "Level 1") {
+//            SceneManager.LoadScene("Level 2");
+//        } else {
+//            SceneManager.LoadScene("Micah's test scene"); //change to title of next scene to link
+//        }
+//    }
+//    IEnumerator Example()
+//    {
+//        Debug.Log("waiting");
+//        yield return new WaitForSecondsRealtime(100f);
+
+//    }
+//}
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,20 +46,47 @@ using UnityEngine.SceneManagement;
 
 public class nextLevelcolideScript : MonoBehaviour
 {
+    //GameObject player;
     private Scene scene;
+    private bool collided = false;
     void Start()
     {
         scene = SceneManager.GetActiveScene();
     }
-
-    private void OnTriggerEnter(Collider other)
+    void Update()
     {
-        if (scene.name == "Level 0") {
-            SceneManager.LoadScene("Level 1");
-        } else if (scene.name == "Level 1") {
-            SceneManager.LoadScene("Level 2");
-        } else {
-            SceneManager.LoadScene("Micah's test scene"); //change to title of next scene to link
+        if (collided)
+        {
+            transform.Rotate(new Vector3(0f, 100f, 0f) * Time.deltaTime);
+
         }
+
     }
+
+    IEnumerator OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("rotate");
+            collided = true;
+            yield return new WaitForSecondsRealtime(3f);
+
+      
+            if (scene.name == "Level 0")
+            {
+                SceneManager.LoadScene("Level 1");
+            }
+            else if (scene.name == "Level 1")
+            {
+                SceneManager.LoadScene("Level 2");
+            }
+            else
+            {
+                SceneManager.LoadScene("Micah's test scene"); //change to title of next scene to link
+            }
+        }
+
+      
+    }
+
 }
