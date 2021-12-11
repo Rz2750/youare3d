c3aa34e16974a56c6b251cac7b02015da5f47117
hannahshaby <hannahshaby@gmail.com>
2021-12-11 13:42:44 -0500

    messed with music :(
    a failure :(

--- You Are 3D/Assets/scripts/AudioManager.cs
+++ You Are 3D/Assets/scripts/AudioManager.cs
@@ -1,31 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
-public class AudioManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    public class AudioPlayerManager: MonoBehaviour
-{
-      private static AudioPlayerManager instance = null;
-      public static AudioSource audio;
 
-      private void Awake()
-      {
-          if (instance == null)
-          { 
-               instance = this;
-               DontDestroyOnLoad(gameObject);
-               return;
-          }
-          if (instance == this) return; 
-          Destroy(gameObject);
-      }
-
-      void Start()
-      {
-         audio = GetComponent<AudioSource>();
-         audio.Play();
-      }
-}
+public class MusicClass : MonoBehaviour {
+  public static AudioSource _audioSource;
+  public void Awake()
+  {
+    DontDestroyOnLoad(transform.gameObject);
+    _audioSource = GetComponent<AudioSource>();
+  }
+  public void PlayMusic()
+  {
+    if (_audioSource.isPlaying) return;
+    _audioSource.Play();
+  }
+  public void StopMusic()
+  {
+    _audioSource.Stop();
+  }
+  
 }
