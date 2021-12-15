363de65b1a0266727e6b84a3fc0385dc8faff75d
hannahshaby <hannahshaby@gmail.com>
2021-12-15 11:25:37 -0500

    good riddance to the compass

--- You Are 3D/Assets/scripts/playerPosition.cs
+++ You Are 3D/Assets/scripts/playerPosition.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class playerPosition : MonoBehaviour
 {
@@ -26,14 +27,16 @@ public class playerPosition : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        xpos = player.transform.position.x;
-        ypos = player.transform.position.y;
-        zpos = player.transform.position.z;
+            xpos = player.transform.position.x;
+            ypos = player.transform.position.y;
+            zpos = player.transform.position.z;
+            
+            xdiff = exit.transform.position.x - player.transform.position.x;
+            ydiff = exit.transform.position.y - player.transform.position.y;
+            zdiff = exit.transform.position.z - player.transform.position.z;
+            
+            float diff = (float)Math.Sqrt((xdiff*xdiff)+(ydiff*ydiff)+(zdiff*zdiff));
         
-        xdiff = exit.transform.position.x - player.transform.position.x;
-        ydiff = exit.transform.position.y - player.transform.position.y;
-        zdiff = exit.transform.position.z - player.transform.position.z;
-        
-        playerpositions.text = "x: " + xpos.ToString("#.00") + " y: " + ypos.ToString("#.00") + " z: " + zpos.ToString("#.00") + "\nx: " + xdiff.ToString("#.00") + " y: " + ydiff.ToString("#.00") + " z: " + zdiff.ToString("#.00");
+        playerpositions.text = diff.ToString("#.00");
     }
 }
